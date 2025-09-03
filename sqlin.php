7f5424c76ec85fc5edff6913391d2a23e800de14
nirjarabamania-oss <nirjarabamania@gmail.com>
2025-09-03 14:14:27 +0530

    new change

--- sqlin.php
+++ sqlin.php
@@ -7,7 +7,7 @@ else{
     echo "connected successfully";
 }
 
-$result = mysqli_query($con,"INSERT INTO studentdata (rollno, firstname, lastname, birthdate) VALUES (1, 'nirjara', 'bamania', '2007-01-01')(2,'priyal','ranpariya','2009-05-05')(3,'nandini','dhayani','2004-09-09')");
+$result = mysqli_query($con,"INSERT INTO studentdata (rollno, firstname, lastname, birthdate) VALUES (1, 'nirjara', 'bamania', '2007-01-01'),(2,'priyal','ranpariya','2009-05-05'),(3,'nandini','dhayani','2004-09-09')");
 if (!$result){
     die("data not inserted". mysqli_error($con));
 }
