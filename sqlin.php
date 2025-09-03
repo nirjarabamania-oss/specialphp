4c7547e5ba511762bc7714a8488cb0f4eaa2b16b
nirjarabamania-oss <nirjarabamania@gmail.com>
2025-09-03 14:11:16 +0530

    new file

---
+++ sqlin.php
@@ -0,0 +1,18 @@
+<?php
+$con = mysqli_connect("localhost","root","","student info");
+if (!$con){
+    die("connection failed". mysqli_connect_error());
+}
+else{
+    echo "connected successfully";
+}
+
+$result = mysqli_query($con,"INSERT INTO studentdata (rollno, firstname, lastname, birthdate) VALUES (1, 'nirjara', 'bamania', '2007-01-01')(2,'priyal','ranpariya','2009-05-05')(3,'nandini','dhayani','2004-09-09')");
+if (!$result){
+    die("data not inserted". mysqli_error($con));
+}
+else{
+    echo "data inserted successfully";
+}
+mysqli_close($con);
+?>
