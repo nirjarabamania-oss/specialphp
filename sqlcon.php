5fdf6216516f8a5797a0d64b5c9d2228db2cd064
nirjarabamania-oss <nirjarabamania@gmail.com>
2025-09-03 16:01:39 +0530

    error solve

--- sqlcon.php
+++ sqlcon.php
@@ -7,10 +7,10 @@ else{
     echo "connected successfully";
 }
 $table="create table course(
-    roll_no INT(6) foreign key references studentdata(rollno),
+    rollno INT(6) foreign key (rollno) references studentdata(rollno),
     studid INT(6) PRIMARY KEY,
     semester INT(6) NOT NULL,
-    cousename VARCHAR(30) NOT NULL)";
+    coursename VARCHAR(30) NOT NULL)";
     $result=mysqli_query($con,$table);
    if (!$result){
        die("table not created". mysqli_error( $con));
