7f6bd7da882b0ce7e1c10cddff841fb93f9c9c1b
nirjarabamania-oss <nirjarabamania@gmail.com>
2025-09-04 16:45:08 +0530

    new program

--- join.php
+++ join.php
@@ -2,6 +2,7 @@
 $con = mysqli_connect("localhost","root","","student info");
 if ( $con) {
     echo "connected successfully";
+    echo"<br>";
 }
 else{
     die("connection failed". mysqli_connect_error());
@@ -31,6 +32,16 @@ $result = mysqli_query($con,"select*from enrollment");
 if (mysqli_num_rows($result )) {
     while($row = mysqli_fetch_assoc($result)){
         echo "rollno: ".$row["rollno"]." - studid: ".$row["studid"]." -enrollmentid : ".$row["enrollmentid"]."<br>";
+    }
+    echo "<br>";
+}
+else{  
+    echo "0 results";
+}
+$result = mysqli_query($con,"select course.rollno,course.studid,course.coursename,enrollment.enrollmentid from course inner join enrollment on course.rollno=enrollment.rollno");
+if (mysqli_num_rows($result )) {
+    while($row = mysqli_fetch_assoc($result)){
+        echo "rollno: ".$row["rollno"]." - studid: ".$row["studid"]." -cousename:".$row["coursename"]." - enrollmentid : ".$row["enrollmentid"]."<br>";
     }
     echo "<br>";
 }
