98d862747d169185aeccb63afc9c4639738e6d34
nirjarabamania-oss <nirjarabamania@gmail.com>
2025-09-05 15:59:43 +0530

    new program

--- group.php
+++ group.php
@@ -6,13 +6,13 @@ if($con){
 else{
     die("connection failed". mysqli_connect_error());
 }
-$result = mysqli_query($con, "SELECT fristname, lastname, rollno FROM studentdata GROUP BY fristname,lastname,rollno");
+$result = mysqli_query($con, "SELECT rollno, coursename, studid FROM course GROUP BY rollno");
 if(mysqli_num_rows($result) == 0){ 
     echo "no records found";
 }
 else{
-    while($row = mysqli_fetch_assoc($result)){
-        echo "fristname: ".$row["fristname"]." - lastname: ".$row["lastname"]." - rollno: ".$row["rollno"]."<br>";
+    while($row = mysqli_fetch_assoc($result)){ 
+        echo "rollno".$row["rollno"]."coursname".$row["coursename"]."studid".$row["studid"];
     }
 }
 mysqli_close($con); 
