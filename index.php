8a8ef602a0f821558a2a16c071c271f10d87842d
nirjarabamania-oss <nirjarabamania@gmail.com>
2025-09-05 14:58:01 +0530

    new program

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+$con = mysqli_connect("localhost","root","","student info");
+if ( $con) {
+    echo "connected successfully";
+    echo"<br>";
+}
+else{
+    die("connection failed". mysqli_connect_error());
+}
+$result=mysqli_query($con,"create index index_stud on studentdata(rollno)");
+if ($result) {
+    echo "index created successfully";
+}
+else{
+    die("index not created". mysqli_error($con));
+}
+$result=mysqli_query($con,"select fristname,lastname ,rollno from studentdata where name like 'n%' group by rollno"); 
+if ($result) {
+    echo "query executed successfully";
+    echo"<br>";
+    while($row=mysqli_fetch_assoc($result)){
+        echo "fristname:".$row['fristname']." lastname:".$row['lastname']." rollno:".$row['rollno'];
+        echo"<br>";
+    }
+}
+else{
+    die("query not executed". mysqli_error($con));
+}
+mysqli_close($con);
+?>
