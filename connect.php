7e4cfc89c446f66adb5cc1bd67262f9c71b4680c
nirjarabamania-oss <nirjarabamania@gmail.com>
2025-09-06 14:44:05 +0530

    new program

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+$con = mysqli_connect("localhost","root","","student info");
+if(!$con) {
+    die("mysql error". mysqli_connect_error());
+}
+else {
+    echo"connected succesfully";
+}
+$result = mysqli_query($con,"select * from studentdata");
+if(!$result) {
+    die("mysql error". mysqli_connect_error());
+}
+else {
+    while($row = mysqli_fetch_array($result)) {
+        echo "rollno: ".$row["rollno"]." - Name: ".$row["firstname"]." ".$row["lastname"]." - birthdate: ".$row["birthdate"]."<br>";
+    }
+}
+mysqli_close($con);
+?>
