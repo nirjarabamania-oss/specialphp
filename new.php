ba7b3aa16530feb8d8e1d3f65db7237823ebc711
nirjarabamania-oss <nirjarabamania@gmail.com>
2025-09-07 17:27:04 +0530

    new program

--- new.php
+++ new.php
@@ -26,14 +26,14 @@ else{
         echo "rollno: ".$row["rollno"]." - Name: ".$row["firstname"]." ".$row["lastname"]." - birthdate: ".$row["birthdate"]." - enrollmentid: ".$row["enrollmentid"]."<br>";
     }
 }
-$result = mysqli_query($con,"insert into studentdata (rollno,firstname,lastname,birthdate,enrollmentid) values (4,'Ankita','Shah','2003-05-12',1),(5,'Riya','Patel','2002-11-23',2)");
-if (!$result){
-    die("data not inserted". mysqli_error($con));
-}
-else{
-    echo "data inserted successfully";
-    echo "<br>";
-}
+#$result = mysqli_query($con,"insert into studentdata (rollno,firstname,lastname,birthdate,enrollmentid) values (4,'Ankita','Shah','2003-05-12',1),(5,'Riya','Patel','2002-11-23',2)");
+#if (!$result){
+ #   die("data not inserted". mysqli_error($con));
+#}
+#else{
+ #   echo "data inserted successfully";
+  #  echo "<br>";
+#}
 $result = mysqli_query($con,"select * from studentdata");
 if (!$result){
     die("table not selected". mysqli_error($con));
