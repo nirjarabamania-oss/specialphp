157f4d83e6ff6aa9e7cd0bd4c4f60522324d17ca
nirjarabamania-oss <nirjarabamania@gmail.com>
2025-09-07 17:30:45 +0530

    new program

--- new.php
+++ new.php
@@ -17,15 +17,15 @@ else{
     echo "table updated successfully";
     echo "<br>";
 }
-$result = mysqli_query($con,"select * from studentdata");
-if (!$result){
-    die("table not selected". mysqli_error($con));
-}
-else{
-    while($row = mysqli_fetch_array($result)) {
-        echo "rollno: ".$row["rollno"]." - Name: ".$row["firstname"]." ".$row["lastname"]." - birthdate: ".$row["birthdate"]." - enrollmentid: ".$row["enrollmentid"]."<br>";
-    }
-}
+//$result = mysqli_query($con,"select * from studentdata");
+//if (!$result){
+  //  die("table not selected". mysqli_error($con));
+//}
+//else{
+   // while($row = mysqli_fetch_array($result)) {
+    //    echo "rollno: ".$row["rollno"]." - Name: ".$row["firstname"]." ".$row["lastname"]." - birthdate: ".$row["birthdate"]." - enrollmentid: ".$row["enrollmentid"]."<br>";
+  //  }
+//}
 #$result = mysqli_query($con,"insert into studentdata (rollno,firstname,lastname,birthdate,enrollmentid) values (4,'Ankita','Shah','2003-05-12',1),(5,'Riya','Patel','2002-11-23',2)");
 #if (!$result){
  #   die("data not inserted". mysqli_error($con));
@@ -43,7 +43,7 @@ else{
         echo "rollno: ".$row["rollno"]." - Name: ".$row["firstname"]." ".$row["lastname"]." - birthdate: ".$row["birthdate"]." - enrollmentid: ".$row["enrollmentid"]."<br>";
     }
 }   
-$result = mysqli_query($con,"delete from studentdata where rollno=5");
+$result = mysqli_query($con,"delete from studentdata where rollno=4");
 if (!$result){
     die("data not deleted". mysqli_error($con));
 }
