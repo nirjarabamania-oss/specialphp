8ba19d7e7d1356e0c8cff77949d6a0a7df8c5ecd
nirjarabamania-oss <nirjarabamania@gmail.com>
2025-09-07 16:44:04 +0530

    new program

---
+++ new.php
@@ -0,0 +1,17 @@
+<?php
+$con = mysqli_connect("localhost","root","","student info");
+if (!$con){
+    die("connection failed". mysqli_connect_error());
+}
+else{
+    echo "connected successfully";
+}
+$result = mysqli_query($con,"alter table studentdata add column enrollmentid int(6), foreign key refrence enrollment(enrollmentid)");
+if (!$result){
+    die("table not altered". mysqli_error($con));
+}
+else{
+    echo "table altered successfully";
+}
+mysqli_close($con);
+?>
